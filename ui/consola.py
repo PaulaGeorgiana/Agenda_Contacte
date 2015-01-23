cb2469fec9aefc121a532e2009fdd39718ab7f4e
PaulaGeorgiana <eu_paula36@yahoo.com>
2015-01-23 16:26:33 +0200

    La functia de afisare, primesc o eroare de genu: "TypeError: 'method' object is not iterable"

--- ui/consola.py
+++ ui/consola.py
@@ -3,6 +3,7 @@ Created on Jan 22, 2015
 
 @author: Georgiana
 '''
+from domain.ContactDom import Contact
 
 class Ui():
     def __init__(self, ctrl):
@@ -15,14 +16,23 @@ class Ui():
         grup= input("Introduceti grupul din care va face parte: ")
         
         self.ctrl.adauga_la_agenda(id_cont, nume, nr_tel, grup)
+        
+    def afiseaza_contacte(self):
+        lista=self.ctrl.getAllCtrl() 
+        for contact in lista:
+            print(self.ctrl.getName(), self.ctrl.getNr_telefon()) 
+        
     def start_ui(self):
         while True:
             print("1. adauga contact")
+            print("2. afiseaza toate contactele")
             print("x. exit")
             
             cmd= input("Dati comanda: ")
             
-        if cmd == 1:
-            self.adauga()
-        if cmd == 'x' :
-            break
+            if cmd == "1":
+                self.adauga()
+            if cmd == 'x' :
+                break
+            if cmd == '2':
+                self.afiseaza_contacte()
